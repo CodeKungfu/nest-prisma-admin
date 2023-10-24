e437d2f96357ed04f11cc5d65b7e236fac59b390
mike <likeming@hqz.net.cn>
2023-10-24 10:48:37 +0800

    wip: refactor step 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,58 @@
+import './polyfill';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
+import { getConfiguration } from './config/configuration';
+import { AdminModule } from './modules/admin/admin.module';
+import { SharedModule } from './shared/shared.module';
+import { MissionModule } from './mission/mission.module';
+import { WSModule } from './modules/ws/ws.module';
+import { LoggerModule } from './shared/logger/logger.module';
+import { WinstonLogLevel } from './shared/logger/logger.interface';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [getConfiguration],
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+    }),
+    BullModule.forRoot({}),
+    // custom logger
+    LoggerModule.forRootAsync(
+      {
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => {
+          return {
+            level: configService.get<WinstonLogLevel>('logger.level'),
+            consoleLevel: configService.get<WinstonLogLevel>(
+              'logger.consoleLevel',
+            ),
+            timestamp: configService.get<boolean>('logger.timestamp'),
+            maxFiles: configService.get<string>('logger.maxFiles'),
+            maxFileSize: configService.get<string>('logger.maxFileSize'),
+            disableConsoleAtProd: configService.get<boolean>(
+              'logger.disableConsoleAtProd',
+            ),
+            dir: configService.get<string>('logger.dir'),
+            errorLogName: configService.get<string>('logger.errorLogName'),
+            appLogName: configService.get<string>('logger.appLogName'),
+          };
+        },
+        inject: [ConfigService],
+      },
+      // global module
+      true,
+    ),
+    // custom module
+    SharedModule,
+    // mission module
+    MissionModule.forRoot(),
+    // application modules import
+    AdminModule,
+    // websocket module
+    WSModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,71 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  HttpStatus,
+  Logger,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+// import { ConfigService } from '@nestjs/config';
+import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
+import { ApiExceptionFilter } from './common/filters/api-exception.filter';
+import { ApiTransformInterceptor } from './common/interceptors/api-transform.interceptor';
+import { setupSwagger } from './setup-swagger';
+import { LoggerService } from './shared/logger/logger.service';
+// import { SocketIoAdapter } from '@/modules/ws/socket-io.adapter';
+
+const SERVER_PORT = process.env.SERVER_PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      bufferLogs: true,
+    },
+  );
+  app.enableCors();
+  // 给请求添加prefix
+  // app.setGlobalPrefix(PREFIX);
+  // custom logger
+  app.useLogger(app.get(LoggerService));
+  // validate
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new UnprocessableEntityException(
+          errors
+            .filter((item) => !!item.constraints)
+            .flatMap((item) => Object.values(item.constraints))
+            .join('; '),
+        );
+      },
+    }),
+  );
+  // execption
+  app.useGlobalFilters(new ApiExceptionFilter(app.get(LoggerService)));
+  // api interceptor
+  app.useGlobalInterceptors(new ApiTransformInterceptor(new Reflector()));
+  // websocket
+  // app.useWebSocketAdapter(new SocketIoAdapter(app, app.get(ConfigService)));
+  // swagger
+  setupSwagger(app);
+  // start
+  await app.listen(SERVER_PORT, '0.0.0.0');
+  const serverUrl = await app.getUrl();
+  Logger.log(`api服务已经启动,请访问: ${serverUrl}`);
+  Logger.log(`API文档已生成,请访问: ${serverUrl}/${process.env.SWAGGER_PATH}/`);
+  Logger.log(
+    `ws服务已经启动,请访问: http://localhost:${process.env.WS_PORT}${process.env.WS_PATH}`,
+  );
 }
+
 bootstrap();
