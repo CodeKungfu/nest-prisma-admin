005437be768a75e6895a41ec848951ed154f19e9
keming <likeming@hqz.net.cn>
2023-10-30 14:25:45 +0800

    fix: permmenu api bug

--- src/modules/admin/system/role/role.service.ts
+++ src/modules/admin/system/role/role.service.ts
@@ -300,7 +300,7 @@ export class SysRoleService {
     });
     if (!isEmpty(result)) {
       return map(result, (v) => {
-        return v.roleId;
+        return v.role_id;
       });
     }
     return [];
